d99841a39f36efd84ab3df603504813e5d3c396e
KimTae-wann <xodhks9711@naver.com>
2022-12-01 21:47:36 +0900

    로그인 추가

--- app/src/main/java/com/example/machumbub/RealSignupActivity.java
+++ app/src/main/java/com/example/machumbub/RealSignupActivity.java
@@ -1,5 +1,6 @@
 package com.example.machumbub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 //툴바 임포트
@@ -11,8 +12,21 @@ import android.view.MenuItem;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RealSignupActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;     // 파이어베이스 인증
+    private DatabaseReference mDatabaseRef; // 실시간 데이터베이스
+    private EditText mEtEmail, mEtPwd;      // 로그인 입력필드
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +39,32 @@ public class RealSignupActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //뒤로가기버튼
         getSupportActionBar().setTitle("로그인"); //툴바 제목
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("Machumbub");
+        mEtEmail = findViewById(R.id.et_email);
+        mEtPwd =findViewById(R.id.et_pwd);
+
         //공부하기 버튼
         Button Startbutton = (Button) findViewById(R.id.startb);
         Startbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainLevelActivity.class);
-                startActivity(intent);
+                // 로그인 요청
+                String strEmail = mEtEmail.getText().toString();
+                String strPwd = mEtPwd.getText().toString();
+
+                mFirebaseAuth.signInWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(RealSignupActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            // 로그인 성공
+                            Intent intent = new Intent(getApplicationContext(), MainLevelActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(RealSignupActivity.this, "로그인에 실패하셨습니다", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
